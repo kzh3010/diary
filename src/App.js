c5ef2df26f21846e099266aed05b6b8de39bae6f
kzh3010 <zcvpal2@naver.com>
2023-04-24 17:44:35 +0900

    React List-Rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  // 이 배열을 DiaryList에게 props으로 전달
+  // diaryList = {dummyList} -> DiaryList에 전달
+  {
+    id: 1,
+    author: "kzh",
+    content: "HI 1",
+    emotion: 5,
+    created_date: new Date().getTime(), // 생성날짜(현재 시간 기준)in milliseconds
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "HI 2",
+    emotion: 4,
+    created_date: new Date().getTime(), // 생성날짜(현재 시간 기준)in milliseconds
+  },
+  {
+    id: 3,
+    author: "일지매",
+    content: "HI 3",
+    emotion: 2,
+    created_date: new Date().getTime(), // 생성날짜(현재 시간 기준)in milliseconds
+  },
+  {
+    id: 4,
+    author: "박씨",
+    content: "HI 4",
+    emotion: 1,
+    created_date: new Date().getTime(), // 생성날짜(현재 시간 기준)in milliseconds
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,26 @@
+import DiaryItem from "./DiaryItem.js";
+
+const DiaryList = ({ diaryList }) => {
+  //{diaryList} = props을 받아온거
+  console.log(diaryList);
+
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// 만약 prop이 undefined로 내려올수 있으니 defaultProps를 이용하여
+// 기본값 설정
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
